bf70428bacacb9582e6667c418337d80a7e2085d
jiahao <a32112354jhj@gmail.com>
2019-05-24 18:02:50 +0800

    month

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
 import React from 'react';
 import logo from './logo.svg';
 import './style.css';
+import classNames from 'classnames';
+import moment from 'moment';
 
 class App extends React.Component {
 
-  state={
-    
+
+  state = {
+    list_style: false,
+    initYearMonth: '201705', //當前月份
+    initYearMonth_next: '201706',
+    initYearMonth_prev: '201704',
+    data : {
+      "guaranteed": true, // {boolean}
+      "date": "2016/12/15", // {string} YYYY/MM/DD
+      "price": "234567", // {string|number} XXXXXX | 近期上架
+      "availableVancancy": 0, // {number}
+      "totalVacnacy": 20, // {number}
+      "status": "報名" // {string} 報名(#24a07c) | 後補(#24a07c) | 預定(#24a07c) | 截止(#ff7800) | 額滿(#ff7800) | 關團(#ff7800)
+    },
   }
+
+  // 列表模式切換
+  ListStatus = () => {
+    if (this.state.list_style === true) {
+      this.setState({
+        list_style: false,
+      });
+    }
+    else if (this.state.list_style === false) {
+      this.setState({
+        list_style: true,
+      });
+    }
+  }
+
+  // 年月處理
+  YearMonth = () => {
+    
+    let month_li=[];
+
+    let YearMonth=this.state.data.map((item,key)=>{
+      month_li.push(item.date.substr(0,7));
+    }); 
+
+    let date_filter=month_li.filter(function(element,index,arr){
+      return arr.indexOf(element) === index;
+    }).sort();
+    
+    // console.log(date_filter);
+    
+    // 取得月份天數
+    console.log(moment("201905", "YYYYMM").daysInMonth() );
+  }
+
+  // componentDidMount=====================
+  componentDidMount() {
+    
+    fetch(
+      '/json/data1.json'
+    )
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data);
+        this.setState({
+          data: data,
+        });
+        this.YearMonth();
+      })
+      .catch(e => console.log('錯誤:', e));
+  }
+  // componentDidMount(E)===================
 
   render() {
     return (
       <div className="App">
 
         {/* 切換列表模式(.list_sty) */}
-        <div className="calendar_box">
-          <a href="javascript:;" className="list_switch">
-            <i class="fas fa-list"></i>
+        <div className={classNames("calendar_box", { list_sty: this.state.list_style === true })}>
+          <a href="javascript:;" className="list_switch" onClick={this.ListStatus}>
+            <i className="fas fa-list"></i>
             切換列表顯示
-        </a>
+          </a>
 
           {/* 日期選擇 */}
           <div className="switch_bar">
@@ -66,7 +131,7 @@ class App extends React.Component {
             </div>
             <div className="day_box has_date has_data">
               <p className="day">2<span className="week_day sunday">星期日</span></p>
-              
+
               <p className="status alternate">候補</p>
               <p className="available">可賣:<span>20</span></p>
               <p className="total">團位:<span>35</span></p>
@@ -93,7 +158,6 @@ class App extends React.Component {
             </div>
             <div className="day_box no_date">
             </div>
-
           </div>
 
         </div>
@@ -103,5 +167,4 @@ class App extends React.Component {
 }
 
 export default App;
-
 
