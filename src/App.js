38b5eee8b33bd9d848347d0b58c1a1a930ca4ade
jiahao <a32112354jhj@gmail.com>
2019-05-28 18:09:21 +0800

    190528_upd

--- src/App.js
+++ src/App.js
@@ -4,23 +4,27 @@ import './style.css';
 import classNames from 'classnames';
 import moment from 'moment';
 
-class App extends React.Component {
 
+class App extends React.Component {
+  weekList = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
   constructor(props) {
     super(props)
+
+    // 資料名----------------------------
+
     this.state = {
       list_style: false,
-      initYearMonth: '201902', //當前年月
+      initYearMonth: '201709', //當前年月
       initYearMonth_next: '201706',
       initYearMonth_prev: '201704',
-      data: {
+      data: [{
         "guaranteed": true, // {boolean}
         "date": "2016/12/15", // {string} YYYY/MM/DD
         "price": "234567", // {string|number} XXXXXX | 近期上架
         "availableVancancy": 0, // {number}
         "totalVacnacy": 20, // {number}
         "status": "報名" // {string} 報名(#24a07c) | 後補(#24a07c) | 預定(#24a07c) | 截止(#ff7800) | 額滿(#ff7800) | 關團(#ff7800)
-      },
+      }],
       getDays: 0,      //當前月份天數
       FirstDayWeek: 0,//當前月份第一天星期
       days: [],       //日期欄位
@@ -68,15 +72,14 @@ class App extends React.Component {
     }, () => {
       _this.calendarDays()
     });
-    console.log(this.state.getDays + ':::' + this.state.FirstDayWeek);
   }
 
   // 日曆日期===================================
   calendarDays = () => {
     let days_nd = []; //空日期
     let days = [];    //日曆日期
-    let days_nda =[]; //補空日期
-    let vacancy = 42-(parseInt(this.state.getDays)+parseInt(this.state.FirstDayWeek));
+    let days_nda = []; //補空日期
+    let vacancy = 42 - (parseInt(this.state.getDays) + parseInt(this.state.FirstDayWeek));
 
     for (var i = 0; i < this.state.FirstDayWeek; i++) {
       days_nd[i] = i;
@@ -88,11 +91,11 @@ class App extends React.Component {
       if (x < 10) {
         n = "0" + n;
       }
-      days[x] = n;
+      days[x] = this.state.initYearMonth + n;
     }
 
-     // 塞入空的天數
-     for (var y = 1; y <= vacancy; y++) {
+    // 塞入空的天數
+    for (var y = 1; y <= vacancy; y++) {
       days_nda[y] = y;
     }
 
@@ -101,11 +104,9 @@ class App extends React.Component {
     this.setState({
       days_nd: days_nd,
       days: days,
-      days_nda:days_nda,
+      days_nda: days_nda,
     }, () => {
-      console.log('NoData:' + this.state.days_nd);
       console.log('Date:' + this.state.days);
-      console.log('Date:' + vacancy);
     });
 
     return;
@@ -131,10 +132,53 @@ class App extends React.Component {
 
   // componentDidMount(E)===================
 
-  render() {
+  // 塞入月曆內容===========================
+  findData = (item) => {
 
-    // this.YearMonth();
-    // this.calendarDays();
+    let row = this.state.data.find((item2) => {
+      return item2.date == moment(item, 'YYYYMMDD').format('YYYY/MM/DD')
+    })
+
+    if (row) {
+      return (
+        <div className="day_box has_date has_data">
+          <p className="day">{item.substr(6, 8)}
+            <span className={moment(item).format('d') === '0' ? "week_day sunday" : "week_day"}>{this.weekList[moment(item).format('d')]}</span>
+          </p>
+          <p className={"status " + this.statusColor(row.status)}>{row.status}</p>
+          <p className="available">可賣:<span>{row.availableVancancy}</span></p>
+          <p className="total">團位:<span>{row.totalVacnacy}</span></p>
+          <p className="price">${row.price.toLocaleString()}</p>
+
+          <p className={row.guaranteed ? "guaranteed" : "guaranteed hide"}>成團</p>
+        </div>
+      )
+    }
+
+    return (
+      <div className="day_box has_date no_data">
+        <p className="day">{item.substr(6, 8)}
+          <span className={moment(item).format('d') === '0' ? "week_day sunday" : "week_day"}>{this.weekList[moment(item).format('d')]}</span>
+        </p>
+      </div>
+    );
+  }
+
+  // 狀態樣式=================================
+  statusColor = (status) => {
+    let statusClass = ['sign', 'alternate', 'cutoff'];
+    if (status === "後補" || status === "請洽專員") {
+      return statusClass[1];
+    }
+    else if (status === "報名" || status === "預定") {
+      return statusClass[0];
+    }
+    else if (status === "額滿" || status === "關團" || status === "截止") {
+      return statusClass[2];
+    }
+  }
+
+  render() {
 
     return (
       <div className="App">
@@ -146,7 +190,7 @@ class App extends React.Component {
             切換列表顯示
           </a>
 
-          {/* 日期選擇 */}
+          {/* 月份選擇 */}
           <div className="switch_bar">
             <button type="button" className="ctrl_btn prev_btn">
             </button>
@@ -167,19 +211,17 @@ class App extends React.Component {
 
           {/* 星期 */}
           <ul className="week_bar">
-            <li>星期日</li>
-            <li>星期一</li>
-            <li>星期二</li>
-            <li>星期三</li>
-            <li>星期四</li>
-            <li>星期五</li>
-            <li>星期六</li>
+            {
+              this.weekList.map((item, key) => {
+                return (
+                  <li key={key}>{item}</li>
+                )
+              })
+            }
           </ul>
 
           {/* 日期 */}
 
-
-          {/* HTML樣板 */}
           <div className="calendar_tb">
             {/* 空(前) */}
             {
@@ -192,15 +234,18 @@ class App extends React.Component {
             }
 
             {
+              this.state.data.map((item, index, arr) => {
+                index = item["date"];
+                if (index === "2017/09/09") {
+                  console.log(item["price"])
+                }
+              })
+            }
+
+            {
               this.state.days.map((item, key) => {
                 return (
-                  <div className="day_box has_date has_data" key={key}>
-                    <p className="day">{item}<span className="week_day">星期一</span></p>
-                    <p className="status">報名</p>
-                    <p className="available">可賣:<span>20</span></p>
-                    <p className="total">團位:<span>35</span></p>
-                    <p className="price">$28,000</p>
-                  </div>
+                  this.findData(item)
                 )
               })
             }
@@ -215,6 +260,7 @@ class App extends React.Component {
               })
             }
 
+            {/* HTML樣板 */}
             {/* <div className="day_box no_date">
             </div>
             <div className="day_box no_date">
